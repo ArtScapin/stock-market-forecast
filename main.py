8fd47cee9b656a308551da3cb89ffab428ad8408
Arthur <arthurscapin19@gmail.com>
2024-10-16 22:21:07 -0300

    feat: verificação de existência de tickers na B3 e ajustes no menu

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from providers.collectingData import downloadStockMarketData
 
+def getInputWithDefault(prompt, default):
+    userInput = input(prompt)
+    return userInput if userInput else default
+
 option = 1
 while option != 0:
     print("--------Serviços--------")
-    print("1- Download de dados pela API Yahoo Finance.")
+    print("1- Download de dados.")
     print("0- Sair.")
     option = int(input("Número do serviço: "))
 
+    print("\n--------------------------------------------------------------------------------\n")
     if option == 1:
-        print("\n--------------------------------------------------------------------------------\n")
         print("Quais são os tickers que deseja baixar dados?")
         print("É possível baixar dados de mais de um ticker por vez, separando-os com vírgula.")
         tickers = input("Tickers: ")
 
-        tickers = tickers.replace(" ", "").split(",")
-        dateStart = input("\nQual é a data inicial do período que deseja baixar os dados? (YYYY-MM-DD): ")
-        dateEnd = input("Qual é a data final do período que deseja baixar os dados? (YYYY-MM-DD): ")
+        tickers = tickers.replace(" ", "").replace(".SA", "").split(",")
+
+        dateStart = getInputWithDefault("\nQual é a data inicial do período que deseja baixar os dados? (YYYY-MM-DD): ", '2020-01-01')
+        dateEnd = getInputWithDefault("Qual é a data final do período que deseja baixar os dados? (YYYY-MM-DD): ", '2023-12-31')
 
         downloadStockMarketData(tickers, dateStart, dateEnd)
 
-        print("\n--------------------------------------------------------------------------------\n")
+    print("\n--------------------------------------------------------------------------------\n")
--- providers/collectingData.py
+++ providers/collectingData.py
@@ -1,18 +1,29 @@
 import yfinance
-
 from providers.databaseConnection import openDatabaseConnection
 
 
 def downloadStockMarketData(tickers, start, end):
+    tickers =  [ticker + '.SA' for ticker in tickers]
     for ticker in tickers:
-        data = yfinance.download(ticker, start, end)
-        if not data.empty:
-            saveStockMarketDataOnDatabase(data, ticker)
+        if isB3Ticker(ticker):
+            data = yfinance.download(ticker, start, end)
+            ticker = ticker.replace('.SA', '')
+            if not data.empty:
+                saveStockMarketDataOnDatabase(data, ticker)
+            else:
+                print(f'Sem dados para {ticker}.')
         else:
-            print(f'No data for {ticker}')
+            print(f'{ticker} não encontrado na listagem de ativos da B3.')
 
 def saveStockMarketDataOnDatabase(data, ticker):
     con = openDatabaseConnection()
-    tableName = ticker.replace(' ', '_')
+    tableName = ticker.replace(' ', '_') + '_RAW'
     data.to_sql(tableName, con, if_exists='replace', index=True)
-    print(f'Data from {ticker} saved in table {tableName}')
+    print(f'Dados de {ticker} salvos na tabela {tableName}.')
+
+def isB3Ticker(ticker):
+    tickerInfo = yfinance.Ticker(ticker)
+    try:
+        return tickerInfo.info['exchange'] == 'SAO'
+    except KeyError:
+        return False
