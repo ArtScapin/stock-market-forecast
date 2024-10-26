0a245b58a5fafff035f44010d652aec89673305e
Arthur <arthurscapin19@gmail.com>
2024-10-26 20:02:14 -0300

    fix: Removendo variáveis não utilizadas

--- providers/modelLSTM.py
+++ providers/modelLSTM.py
@@ -13,9 +13,9 @@ def analyzingDataWithLSTM(ticker, predictionType):
 
     scaledData, scaler = applyMinMaxScaling(closingPrices)
 
-    lstm, XTest, yTest = createAndTrainModel(scaledData)
+    lstm, sequencesTest = createAndTrainModel(scaledData)
 
-    predictedPrices, realPrices = makePredictions(lstm, XTest, yTest, scaler, predictionType)
+    predictedPrices = makePredictions(lstm, sequencesTest, scaler, predictionType)
 
     predictedDates = dataframe[['Date']].values
     predictedDates = predictedDates[len(predictedDates)-len(predictedPrices):]
@@ -42,7 +42,7 @@ def createAndTrainModel(dataframe):
 
     trainSize = int(len(sequences) * 0.8)
     sequencesTrain, sequencesTest = sequences[:trainSize], sequences[trainSize:]
-    valuesTrain, valuesTest = values[:trainSize], values[trainSize:]
+    valuesTrain = values[:trainSize]
 
     lstm = Sequential([
         LSTM(units=50, return_sequences=True, input_shape=(sequencesTrain.shape[1], sequencesTrain.shape[2])),
@@ -56,25 +56,24 @@ def createAndTrainModel(dataframe):
 
     lstm.fit(sequencesTrain, valuesTrain, epochs=50, batch_size=32)
 
-    return lstm, sequencesTest, valuesTest
+    return lstm, sequencesTest
 
 
-def makePredictions(model, XTest, yTest, scaler, predictionType):
+def makePredictions(model, sequencesTest, scaler, predictionType):
     predictedPrices = []
     if predictionType == 1:
-        sequence = XTest[0]
-        for _ in range(len(yTest)):
+        sequence = sequencesTest[0]
+        for _ in range(len(sequencesTest)):
             prediction = model.predict(sequence.reshape(1, sequence.shape[0], sequence.shape[1]))
             predictedPrices.append(prediction[0])
             sequence = np.roll(sequence, -1, axis=0)
             sequence[-1] = prediction
     else:
-        predictedPrices = model.predict(XTest)
+        predictedPrices = model.predict(sequencesTest)
 
     predictedPricesFull = scaler.inverse_transform(predictedPrices)
-    yTestFull = scaler.inverse_transform(yTest)
 
-    return predictedPricesFull, yTestFull
+    return predictedPricesFull
 
 
 def save(predictedPrices, predictedDates, predictionType, ticker):
